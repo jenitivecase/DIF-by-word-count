b14d1f45cb1d5fa9d57b12591a1a0a428f106304
jenitivecase <jbrussow@gmail.com>
2017-09-01 16:32:01 -0500

    cleanup start

--- DIF_by_WC_analysis.R
+++ DIF_by_WC_analysis.R
@@ -5,11 +5,16 @@ sapply(needed_packages, load_packages)
 
 item_content <- read.xlsx("K:/AscendKC/Corp/R_and_D/1-USERS/Jennifer Brussow/Outcome Modeling/Outcome_analysis/Reporting/item_content_from_QBTB-db.xlsx")
 
-responses <- readRDS("K:/AscendKC/Corp/R_and_D/1-USERS/Jennifer Brussow/Outcome Modeling/Outcome_analysis/Analysis/item-level_RN-2016CP_outcome-scores.rds")
+responses <- readRDS("CP2016_responses.rds")
 
-names(responses)[1] <- gsub("ï..", "", names(responses)[1])
+names(responses)[1] <- gsub("Ã¯..", "", names(responses)[1])
 
 
 responses <- responses %>% 
-  select(BookletID, qbtbQuestionID, IsCorrect) %>%
-  spread(qbtbQuestionID, IsCorrect)
+  select(BookletID, qbtbQuestionID, IsCorrect, LanguageID) %>%
+  mutate(LanguageID = case_when(LanguageID == 0 ~ 0,
+                                is.null(LanguageID) ~ 2,
+                                LanguageID != 0 ~ 1)) %>%
+  filter(LanguageID != 2) %>%
+  spread(qbtbQuestionID, IsCorrect) %>%
+  unique()
