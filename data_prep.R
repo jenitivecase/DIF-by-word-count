fe607d73ba699b2b24c7d59b98d3a737ad135663
jenitivecase <jbrussow@gmail.com>
2017-09-05 13:42:01 -0500

    updated to new dataset, fix names here

--- data_prep.R
+++ data_prep.R
@@ -1,7 +1,11 @@
-responses <- read.table("CP2016_responses.txt",
-                            sep = ",", header = TRUE, as.is = TRUE,
-                            strip.white = TRUE, fill = TRUE,
-                            blank.lines.skip = TRUE)
+# responses <- read.table("CP2016_responses_20170901.txt",
+#                             sep = ",", header = TRUE, as.is = TRUE,
+#                             strip.white = TRUE, fill = TRUE,
+#                             blank.lines.skip = TRUE)
 
-saveRDS(responses, "CP2016_responses.rds")
+responses <- read.csv("CP2016_responses_20170905.csv", stringsAsFactors = FALSE)
+
+names(responses)[1] <- gsub("ï..", "", names(responses)[1])
+
+saveRDS(responses, "CP2016_responses_20170905.rds")
 gc()
