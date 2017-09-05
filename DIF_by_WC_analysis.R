d447b050ca4ccf543936265943526c751ad072af
jenitivecase <jbrussow@gmail.com>
2017-09-05 13:58:50 -0500

    finally, clean data.

--- DIF_by_WC_analysis.R
+++ DIF_by_WC_analysis.R
@@ -5,9 +5,7 @@ sapply(needed_packages, load_packages)
 
 item_content <- read.xlsx("K:/AscendKC/Corp/R_and_D/1-USERS/Jennifer Brussow/Outcome Modeling/Outcome_analysis/Reporting/item_content_from_QBTB-db.xlsx")
 
-responses <- readRDS("CP2016_responses.rds")
-
-names(responses)[1] <- gsub("ï..", "", names(responses)[1])
+responses <- readRDS("CP2016_responses_20170905.rds")
 
 responses <- responses %>% 
   select(BookletID, qbtbQuestionID, IsCorrect, LanguageID) %>%
@@ -17,11 +15,12 @@ responses <- responses %>%
   filter(LanguageID != 2) %>%
   filter(!is.na(IsCorrect)) %>%
   unique() %>%
-  #these last three lines are to avoid issues with qbtbQuestionID 18 having multiple responses
-  #7598 students had both correct and incorrect responses logged for that question
   group_by(BookletID, qbtbQuestionID) %>% 
   filter(length(unique(IsCorrect)) == 1) %>% 
   ungroup() 
 
 
+responses_wide <- responses %>%
+  spread(qbtbQuestionID, IsCorrect) %>%
+  arrange(BookletID)
 
