4f515ff06dd2d93eb46f6b73192d11bc006881ab
jenitivecase <jbrussow@gmail.com>
2017-09-05 14:26:44 -0500

    adding analysis section

--- DIF_by_WC_analysis.R
+++ DIF_by_WC_analysis.R
@@ -1,6 +1,9 @@
+################################################################################
+#### SETUP #####################################################################
+################################################################################
 source("K:/AscendKC/Corp/R_and_D/1-USERS/Jennifer Brussow/options.R")
 
-needed_packages <- c("mirt")
+needed_packages <- c("mirt", "rstan", "stringr")
 sapply(needed_packages, load_packages)
 
 item_content <- read.xlsx("K:/AscendKC/Corp/R_and_D/1-USERS/Jennifer Brussow/Outcome Modeling/Outcome_analysis/Reporting/item_content_from_QBTB-db.xlsx")
@@ -17,10 +20,51 @@ responses <- responses %>%
   unique() %>%
   group_by(BookletID, qbtbQuestionID) %>% 
   filter(length(unique(IsCorrect)) == 1) %>% 
-  ungroup() 
+  ungroup() %>%
+  arrange(qbtbQuestionID)
+
+nwords <- function(string, pseudo=F){
+  ifelse( pseudo, 
+          pattern <- "\\S+", 
+          pattern <- "[[:alpha:]]+" 
+  )
+  str_count(string, pattern)
+}
+
+item_content <- item_content %>%
+  group_by(QuestionID) %>%
+  mutate(WC = nwords(unique(stem), pseudo = T)) %>%
+  ungroup() %>%
+  arrange(QuestionID)
+
+################################################################################
+#### ANALYSIS ##################################################################
+################################################################################
+
+precomp <- stanc(file = "explanatory_DIF.stan")
+precomp_model <- stan_model(stanc_ret = precomp)
+
+n_people <- length(unique(responses$BookletID))
+n_items <- length(unique(responses$qbtbQuestionID))
+n_observations <- nrow(responses)
+respondentid <- responses$BookletID
+itemid <- responses$qbtbQuestionID
+response <- responsese$IsCorrect
+group_long <- responses$LanguageID
+group <- unique(responses[, c("BookletID", "LanguageID")])
+DIFpredict <- item_content$WC
 
 
-responses_wide <- responses %>%
-  spread(qbtbQuestionID, IsCorrect) %>%
-  arrange(BookletID)
 
+dat_long <- list("n_people", "n_items", "n_observations", "respondentid", 
+                   "itemid", "response", "group", "group_long",
+                   "DIFpredict")
+
+analysis <- sampling(precomp_model, data = dat_long,
+                     iter = 12000, warmup = 5000, chains = 2, 
+                     verbose = TRUE, cores = 2)
+
+params_summary <- summary(analysis, pars = c("a", "b", "D", "beta0", "beta1", "mu", 
+                                             "sigma2", "R2", "theta",
+                                             "foc_mean"),
+                          probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary
